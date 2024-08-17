dfb062d8623e0c1ab6e58d5f68ac71c1e3413cc1
Russ <rt@russtaylor.co.uk>
2024-08-17 13:10:41 +0100

    Draws a tank and some vegetation in correct colours

--- entry_russgame.c
+++ entry_russgame.c
@@ -1,3 +1,54 @@
+typedef enum EntityArchetype
+{
+	arch_nil = 0,
+	arch_tank = 1,
+	arch_edith = 2,
+	arch_blossom = 3,
+	arch_veg = 4,
+	arch_building = 5,
+} EntityArchetype;
+
+typedef struct Entity
+{
+	bool is_valid;
+	EntityArchetype arch;
+	Vector2 pos;
+} Entity;
+#define MAX_ENTITY_COUNT 1024
+
+typedef struct World
+{
+	Entity entities[MAX_ENTITY_COUNT];
+} World;
+World *world = 0;
+
+Entity *entity_create()
+{
+	Entity *entity_found = 0;
+	for (int i = 0; i < MAX_ENTITY_COUNT; i++)
+	{
+		Entity *existing_entity = &world->entities[i];
+		if (!existing_entity->is_valid)
+		{
+			entity_found = existing_entity;
+			break;
+		}
+	}
+	assert(entity_found, "No more free entities!");
+	entity_found->is_valid = true;
+	return entity_found;
+}
+
+void entity_destroy(Entity *entity)
+{
+	memset(entity, 0, sizeof(Entity));
+}
+
+void setup_veg(Entity *en)
+{
+	en->arch = arch_veg;
+}
+
 int entry(int argc, char **argv)
 {
 	window.title = STR("Edith & Tank");
@@ -7,12 +58,26 @@ int entry(int argc, char **argv)
 	window.y = 90;
 	window.clear_color = hex_to_rgba(0xffffffff);
 
+	world = alloc(get_heap_allocator(), sizeof(World));
+
 	float64 last_time = os_get_current_time_in_seconds();
 
 	Gfx_Image *player = load_image_from_disk(fixed_string("assets/tank.png"), get_heap_allocator());
 	assert(player, "Couldn't load player image");
 
-	Vector2 player_pos = v2(0, 0);
+	Gfx_Image *edith = load_image_from_disk(fixed_string("assets/edith_front.png"), get_heap_allocator());
+	Gfx_Image *blossom = load_image_from_disk(fixed_string("assets/blossom.png"), get_heap_allocator());
+	Gfx_Image *building = load_image_from_disk(fixed_string("assets/building.png"), get_heap_allocator());
+	Gfx_Image *veg = load_image_from_disk(fixed_string("assets/veg.png"), get_heap_allocator());
+
+	Entity *player_en = entity_create();
+
+	for (int i = 0; i < 10; i++)
+	{
+		Entity *en = entity_create();
+		setup_veg(en);
+		en->pos = v2(i * 100.0, 0.0);
+	}
 
 	while (!window.should_close)
 	{
@@ -31,7 +96,29 @@ int entry(int argc, char **argv)
 
 		draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
 
-		draw_frame.view = m4_make_scale(v3(4, 4, 1.0));
+		draw_frame.view = m4_make_scale(v3(1.0, 1.0, 1.0));
+
+		for (int i = 0; i < MAX_ENTITY_COUNT; i++)
+		{
+			Entity *en = &world->entities[i];
+			if (en->is_valid)
+			{
+				switch (en->arch)
+				{
+				case arch_veg:
+				{
+					Vector2 size = v2(63, 56);
+					Matrix4 xform = m4_scalar(1.0);
+					xform = m4_translate(xform, v3(en->pos.x, en->pos.y, 0));
+					// xform = m4_translate(xform, v3(size.x * -0.5, 0, 0));
+					draw_image_xform(veg, xform, size, COLOR_WHITE);
+				}
+				default:
+				{
+				}
+				}
+			}
+		}
 
 		Vector2 input_axis = v2(0, 0);
 		if (is_key_down('A'))
@@ -52,19 +139,19 @@ int entry(int argc, char **argv)
 		}
 		input_axis = v2_normalize(input_axis);
 
-		player_pos = v2_add(player_pos, v2_mulf(input_axis, 1000.0 * delta_t));
+		player_en->pos = v2_add(player_en->pos, v2_mulf(input_axis, 1000.0 * delta_t));
 
-		Vector2 player_size = v2(409, 281);
+		{
+			Vector2 size = v2(409, 281);
+			Matrix4 xform = m4_scalar(1.0);
+			xform = m4_translate(xform, v3(player_en->pos.x, player_en->pos.y, 0));
+			xform = m4_translate(xform, v3(size.x * -0.5, 0, 0));
+			draw_image_xform(player, xform, size, COLOR_WHITE);
+		}
 
-		Matrix4 xform = m4_scalar(1.0);
-		xform = m4_translate(xform, v3(player_pos.x, player_pos.y, 0));
-		xform = m4_translate(xform, v3(player_size.x * -0.5, 0, 0));
-
-		draw_image_xform(player, xform, player_size, COLOR_GREEN);
-
-		float aspect = (f32)window.width / (f32)window.height;
-		float mx = (input_frame.mouse_x / (f32)window.width * 2.0 - 1.0) * aspect;
-		float my = input_frame.mouse_y / (f32)window.height * 2.0 - 1.0;
+		// float aspect = (f32)window.width / (f32)window.height;
+		// float mx = (input_frame.mouse_x / (f32)window.width * 2.0 - 1.0) * aspect;
+		// float my = input_frame.mouse_y / (f32)window.height * 2.0 - 1.0;
 
 		os_update();
 		gfx_update();
